114b9cf87c87819129a173247f08d66abb365bbc
Lex-ion <killkiisdog@gmail.com>
2023-04-19 20:39:28 +0200

    Přidejte soubory projektu.

---
+++ test colors/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+namespace test_colors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            
+
+            Color c = Color.Firebrick;
+            ConsoleColor wc=0;
+            
+            Color[] rc = new Color[16];
+
+            for (int i = 0; i < rc.Length; i++)
+            {
+                ConsoleColor mc = (ConsoleColor)i;
+                rc[i] = Color.FromName(mc.ToString());
+            }//tohle může být v cyklu pod
+
+
+            int[] scores = new int[rc.Length];
+
+            for (int i = 0; i < scores.Length; i++)// v tomhle
+            {
+
+                int scoreR = Math.Abs(rc[i].R-c.R);
+                int scoreG = Math.Abs(rc[i].G-c.G);
+                int scoreB = Math.Abs(rc[i].B-c.B);
+                scores[i] = scoreR+scoreG+scoreB;
+                
+            }
+            
+            
+
+            int smallest = scores[0];
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] < smallest)
+                {
+                    smallest = scores[i];
+                }
+            }
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (smallest == scores[i])
+                {
+                    wc= (ConsoleColor)i;
+                    break;
+                }
+            }
+            Console.BackgroundColor = wc;
+            Console.Clear();
+
+        }
+    }
+}
